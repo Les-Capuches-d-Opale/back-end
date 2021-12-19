a59022388f0b45f8efb8d9fadf0018a7154756c1
Lilian <lilianouvmmi@gmail.com>
2021-12-19 14:54:32 +0100

    added administrator unit test & init some anothers

--- src/adventurers/adventurers.service.spec.ts
+++ src/adventurers/adventurers.service.spec.ts
@@ -1,12 +1,27 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdventurersService } from './adventurers.service';
+import { Adventurer } from './entities/adventurer.entity';
 
 describe('AdventurersService', () => {
   let service: AdventurersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdventurersService],
+      providers: [
+        AdventurersService,
+        {
+          provide: getModelToken('Adventurer'),
+          useValue: {
+            constructor: jest.fn().mockResolvedValue(Adventurer),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AdventurersService>(AdventurersService);
