50794bf28d6acd2c673180e55ca14dfc99993ebf
marth154 <martin.danvers@gmail.com>
2022-01-06 21:09:58 +0100

    Creation Quest

--- src/quests/quests.service.ts
+++ src/quests/quests.service.ts
@@ -2,13 +2,16 @@ import { Quest } from './entities/quest.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateQuestDto } from './dto/createQuest.dto';
+const mongoose = require('mongoose');
+
 
 @Injectable()
 export class QuestsService {
   constructor(
     @InjectModel(Quest.name)
     private readonly questModel: Model<Quest>,
-  ) {}
+  ) { }
 
   async findAll(): Promise<Quest[]> {
     return await this.questModel
@@ -24,5 +27,16 @@ export class QuestsService {
       .populate('request')
       .populate('groups')
       .exec();
+  }
+
+  async createQuest(createAdventurerDto: CreateQuestDto): Promise<Quest>  {
+    const{request, groups} = createAdventurerDto
+     const quest = new this.questModel({
+      request: new mongoose.Types.ObjectId(request),
+      groups: groups,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    return quest.save({timestamps: true})
   }
 }
