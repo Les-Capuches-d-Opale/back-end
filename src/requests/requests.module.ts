d12d4c53c7c8a948ad75637a50efd3ef1270d73c
marth154 <martin.danvers@gmail.com>
2022-01-06 13:22:49 +0100

    Controller request

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,4 +1,21 @@
+import { RequestSchema } from './entities/request.entity';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Request } from './entities/request.entity';
+import { RequestsController } from './requests.controller';
+import { RequestsService } from './requests.service';
 
-@Module({})
-export class RequestModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Request.name,
+        schema: RequestSchema,
+      },
+    ]),
+  ],
+  providers: [RequestsService],
+  controllers: [RequestsController],
+  exports: [RequestsService]
+})
+export class RequestsModule {}
