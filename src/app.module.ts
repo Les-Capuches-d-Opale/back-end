286578e9aff467dea6e38db1908913d6d7af13a8
marth154 <martin.danvers@gmail.com>
2022-01-06 13:22:49 +0100

    Controller request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { RequestsController } from './requests/requests.controller';
 import { TransactionsController } from './transactions/transactions.controller';
 import { QuestsController } from './quests/quests.controller';
 import { TransactionsModule } from './transactions/transactions.module';
 import { AdventurersModule } from './adventurers/adventurers.module';
 import { ItemsController } from './items/items.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module } from '@nestjs/common';
+import { Module, Req } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AdventurersController } from './adventurers/adventurers.controller';
@@ -16,7 +17,8 @@ import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { QuestsModule } from './quests/quests.module';
-import { RequestModule } from './request/request.module';
+import { RequestsService } from './requests/requests.service';
+import { RequestsModule } from './requests/requests.module';
 
 @Module({
   imports: [
@@ -29,6 +31,7 @@ import { RequestModule } from './request/request.module';
     AdministratorsModule,
     QuestsModule,
     TransactionsModule,
+    RequestsModule
   ],
   controllers: [
     AppController,
@@ -36,7 +39,7 @@ import { RequestModule } from './request/request.module';
     ItemsController,
     QuestsController,
     TransactionsController,
-    RequestModule,
+    RequestsController
   ],
   providers: [
     AppService,
---
+++ src/request/requests.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Request } from './entities/request.entity';
+
+@Injectable()
+export class RequestsService {
+  constructor(
+    @InjectModel(Request.name)
+    private readonly RequestModel: Model<Request>,
+  ) {}
+
+  async findAll(): Promise<Request[]> {
+    return await this.RequestModel.find({}).exec();
+  }
+}
