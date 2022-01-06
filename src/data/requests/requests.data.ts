d7e0a4f3c8ee587c3d4ecb05960d1148c76ed5e4
marth154 <martin.danvers@gmail.com>
2022-01-06 18:53:20 +0100

    Get request

--- src/data/requests/requests.data.ts
+++ src/data/requests/requests.data.ts
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose');
+const items = require('../items/item.data');
+
+const randomItems = (items) => {
+    return items
+        .sort(() => 0.5 - Math.random())
+        .slice(0, Math.floor(Math.random() * 8 + 4));
+};
 
 const requests = [
     {
@@ -7,16 +14,32 @@ const requests = [
         questGiver: "Le prètre",
         bounty: Math.floor(Math.random() * 1000),
         requiredProfiles: [{
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e5"),
+            specificity: {
+                name: 'Archer',
+                description:
+                    "L'archer se consacre à la maîtrise de l'arc. Des années d'entraînement ont perfectionné ses compétences, il s'est affûté jour après jour sur des cibles, à la chasse ou à la guerre, faisant pleuvoir la mort sur les lignes ennemies.",
+                requiredItems: randomItems(items)
+            },
             experience: Math.floor(Math.random() * 10)
         }, {
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e1"),
+            specificity: {
+                name: 'Babare',
+                description:
+                    'Certains barbares sont véritablement sauvages et n’ont presque aucune connaissance des armes modernes. Ils apprennent à éviter les coups et à endurcir leur peau.',
+                requiredItems: randomItems(items),
+            },
             experience: Math.floor(Math.random() * 10)
         }, {
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e4"),
+            specificity: {
+                name: 'Mage',
+                description:
+                    'Ayant été initié à de sombres mystères, le mage a suivi une formation qui lui permet de manipuler le pouvoir des ombres pour renforcer ses incantations.',
+                requiredItems: randomItems(items),
+            },
             experience: Math.floor(Math.random() * 10)
         }],
         awardedExperience: Math.floor(Math.random() * 10),
+        duration: Math.floor(Math.random() * 1000000),
         dateDebut: "2022-10-01"
     },
     {
@@ -25,13 +48,23 @@ const requests = [
         questGiver: "Le mage",
         bounty: Math.floor(Math.random() * 1000),
         requiredProfiles: [{
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e9"),
+            specificity: {
+                name: 'Forgeron',
+                description:
+                    'Armuriers extrêmement talentueux, les prêtres forgerons tirent leur inspiration de leur divinité pour produire les armes et les armures les plus parfaites possible et améliorer l’équipement utilisé par les armées de fidèles.',
+                requiredItems: randomItems(items),
+            },
             experience: Math.floor(Math.random() * 10)
         }, {
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e8"),
+            specificity: {
+                name: 'messagers',
+                description: "C'est juste un gars de La Poste.",
+                requiredItems: randomItems(items),
+            },
             experience: Math.floor(Math.random() * 10)
         }],
         awardedExperience: Math.floor(Math.random() * 10),
+        duration: Math.floor(Math.random() * 1000000),
         dateDebut: "2022-11-03"
     },
     {
@@ -40,25 +73,31 @@ const requests = [
         questGiver: "Le medecin",
         bounty: Math.floor(Math.random() * 1000),
         requiredProfiles: [{
-            _id: new mongoose.Types.ObjectId("5cabe64dcf0d4447fa60f5e2"),
+            specificity: {
+                name: 'Forgeron',
+                description:
+                    'Armuriers extrêmement talentueux, les prêtres forgerons tirent leur inspiration de leur divinité pour produire les armes et les armures les plus parfaites possible et améliorer l’équipement utilisé par les armées de fidèles.',
+                requiredItems: randomItems(items),
+            },
             experience: Math.floor(Math.random() * 10)
         }],
         awardedExperience: Math.floor(Math.random() * 10),
+        duration: Math.floor(Math.random() * 1000000),
         dateDebut: "2022-20-01"
     }]
 
 export = requests.map((request) => {
-  return {
-    name: request.name,
-    description: request.description,
-    pictureUrl:
-      'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg',
-    questGiver: request.questGiver,
-    bounty: request.bounty,
-    duration: request.bounty,
-    requiredProfiles: request.requiredProfiles,
-    awardedExperience: request.awardedExperience,
-    dateDebut: request.dateDebut,
-    status: 'Unassigned',
-  };
+    return {
+        name: request.name,
+        description: request.description,
+        pictureUrl:
+            'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg',
+        questGiver: request.questGiver,
+        bounty: request.bounty,
+        duration: request.duration,
+        requiredProfiles: request.requiredProfiles,
+        awardedExperience: request.awardedExperience,
+        dateDebut: request.dateDebut,
+        status: 'Unassigned',
+    };
 });
