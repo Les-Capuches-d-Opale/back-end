30b3da0057cef11ca1a58c1bfc76329e5fe28a3f
assimmht <assimmht235@gmail.com>
2022-01-14 14:27:13 +0100

    Créer une route avec des paramètres pour filtrer

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -120,6 +120,7 @@ export class RequestsService {
     return req.save({ timestamps: true })
   }
 
+  
   async FilterAll(
     filterRequestQueryDto: FilterRequestQueryDto,
   ): Promise<Request[] | any> {
