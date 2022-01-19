81a9390ece7a697c95f4177ef5bb549cb4f045c6
marth154 <37938187+marth154@users.noreply.github.com>
2022-01-19 22:32:48 +0100

    Correction CO-98

--- src/quests/dto/createQuest.dto.ts
+++ src/quests/dto/createQuest.dto.ts
@@ -1,18 +1,48 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsString } from 'class-validator';
+import { RequiredProfilDto } from './requiredProfile.dto';
 
 export class CreateQuestDto {
-    @ApiProperty({
-        description: 'The ID of the request.',
-        example: '9cabe64dcf0d4447fa60f5e2',
-    })
-    @IsString()
-    readonly request: string;
+  @ApiProperty({
+    description: 'The ID of the request.',
+    example: '9cabe64dcf0d4447fa60f5e4',
+  })
+  @IsString()
+  readonly request: string;
 
-    @ApiProperty({
-        description: 'The list of ID of adventurers.',
-        example: ['7cabe64dcf0d4447fa60f5e4', '7cabe64dcf0d4447fa60f5e3'],
-    })
-    @IsArray()
-    readonly groups: string[]
+  @ApiProperty({
+    description: 'The list of ID of adventurers.',
+    example: [
+      {
+        reqProfile: {
+          id: '5cabe64dcf0d4447fa60f5e8',
+          experience: 0,
+        },
+        adventurer:  '7cabe64dcf0d4447fa60f5e4'
+      },
+      {
+        reqProfile: {
+          id: '5cabe64dcf0d4447fa60f5e4',
+          experience: 5,
+        },
+        adventurer:  '7cabe64dcf0d4447fa60f5e2',
+      },
+      {
+        reqProfile: {
+          id: '5cabe64dcf0d4447fa60f5e6',
+          experience: 3,
+        },
+        adventurer:'5cabe64dcf0d4447fa60f5f0'
+      },
+      {
+        reqProfile: {
+          id: '5cabe64dcf0d4447fa60f5e9',
+          experience: 4,
+        },
+        adventurer: '5cabe64dcf0d4447fa60f5f1'
+      },
+    ],
+  })
+  @IsArray()
+  readonly groups: RequiredProfilDto[];
 }
