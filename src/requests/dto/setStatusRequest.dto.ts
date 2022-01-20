447794aa31f063c7f1eed374be641fe76aaac9f1
marth154 <martin.danvers@gmail.com>
2022-01-20 10:24:29 +0100

    Change PUT route

--- src/requests/dto/setStatusRequest.dto.ts
+++ src/requests/dto/setStatusRequest.dto.ts
@@ -2,14 +2,7 @@ import { QuestStatus } from './../entities/request.entity';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class SetStatusRequestDto {
-    @ApiProperty({
-        description: 'The ID of the request.',
-        example: '9cabe64dcf0d4447fa60f5e2',
-    })
-    @IsString()
-    readonly request: string;
-    
+export class SetStatusRequestDto {    
     @ApiProperty({
         description: "The status to change",
         example: "Rejected"
