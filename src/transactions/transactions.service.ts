7fdc16c155d93ed8a711cc0f2cdf3cc153a3ea1a
assimmht <assimmht235@gmail.com>
2022-01-21 15:06:39 +0100

    Stage before rebase

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -38,4 +38,48 @@ export class TransactionsService {
 
     return transactions;
   }
+
+  async getMore() {
+    const type = 'QuestBounty';
+    const transactions = await this.transactionModel
+      .find({type: { $regex: type, $options: 'i' },})
+      .where({})
+      .exec();
+     
+      return this.calculateAmountTransaction(transactions);
+  }
+
+  async getLess(): Promise<Transaction[] | any> {
+    const type1 = 'Tax';
+    const type2 = 'Purchase';
+    const transactions = await this.transactionModel
+      .find({
+        $or: [
+          {type: { $regex: type1, $options: 'i' }},
+          {type: { $regex: type2, $options: 'i' }}
+      ]})
+      .where({})
+      .exec();
+     
+      return this.calculateAmountTransaction(transactions);
+  }
+ 
+ calculateAmountTransaction = (transactions: any) => {
+    let result = [];
+    let amount = 0;
+    let date = undefined;
+    let index = 0;
+    transactions.map(element => {
+      date = element.date.toLocaleDateString('fr-FR');
+      amount = element.amount;
+      index = result.findIndex(x => x.date === date);
+      if (index === -1) {
+        result.push({date: date, amount: amount});
+       } else {
+        result[index] = {date: date, amount: result[index].amount + amount};
+      }
+    });
+    return result;
+  }
+
 }
