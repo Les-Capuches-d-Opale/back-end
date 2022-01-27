f795bf1e185af6a2b7ccfb77d3d4ad2ccf9a1308
marth154 <37938187+marth154@users.noreply.github.com>
2022-01-27 01:59:48 +0100

    Data

--- src/data/requests/requests.data.ts
+++ src/data/requests/requests.data.ts
@@ -130,6 +130,184 @@ const requests = [
     dateFin: '2022-04-27',
     status: 'Unassigned',
   },
+
+  {
+    _id: new mongoose.Types.ObjectId('9cabe64dcf0d4447fa60f5e6'),
+    name: 'Amnésie',
+    description:
+      'Un ou plusieurs PJ se réveillent sans souvenir récents, et se retrouvent au cœur d’un problème qu’ils ne comprennent pas. Ils doivent trouver la cause de cette amnésie et entre-temps résoudre les problèmes qu’ils découvrent.',
+    questGiver: 'Damso',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e1'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-02-24',
+    dateFin: '2022-03-02',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('9cabe64dcf0d4447fa60f5e7'),
+    name: 'Base secrète',
+    description:
+      'Les PJ, au cours d’un voyage ou d’une exploration, tombent sur un nid de Méchants qui préparent un Truc Méchant. Soit ils trouvent un moyen de rapporter l’information aux Gentils, soit ils s’infiltrent et neutralisent la base eux-mêmes, ou encore une combinaison des deux.',
+    questGiver: 'James Bond',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e3'),
+        experience: Math.floor(Math.random() * 10),
+      },
+
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e6'),
+        experience: Math.floor(Math.random() * 10),
+      },
+
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e5'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-01-27',
+    dateFin: '2022-01-30',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('9cabe64dcf0d4447fa60f5e8'),
+    name: 'Capture du drapeau',
+    description:
+      'Les PJ ont pour mission de s’emparer d’un objectif militaire pour les Gentils. Les Méchants du coin s’y opposent. Le scénario tactique habituel.',
+    questGiver: 'Captaine Flame',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e3'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e1'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-02-10',
+    dateFin: '2022-02-14',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('9cabe64dcf0d4447fa60f5e9'),
+    name: 'Souvenir éternel',
+    description:
+      "Voler / Acheter journal de Lorie à l'antiquaire affable de Noblecour",
+    questGiver: 'Jeune homme mélancolique',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e7'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e8'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e1'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-03-03',
+    dateFin: '2022-03-04',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('cabe64dcf0d4447fa60f5e10'),
+    name: 'Mauvaises graines',
+    description: 'Voler / Acheter du fumier à un homme du village	',
+    questGiver: 'Fermier aimable',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e8'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-02-10',
+    dateFin: '2022-02-13',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('cabe64dcf0d4447fa60f5e11'),
+    name: 'Kaia, la mère des dragons',
+    description:
+      'Scruter / Questionner un fermier sur le Gué Ouest de Ruisseclair puis trouver un oeuf géant dans la zone',
+    questGiver: 'Jeune fille cherchant un oeuf',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e2'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-02-24',
+    dateFin: '2022-02-31',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('cabe64dcf0d4447fa60f5e12'),
+    name: 'Le code des bandits',
+    description:
+      "Guider / Séduire un jeune homme dans la taverne d'Ombrelle OU : Scruter / Questionner la Matrone de l'orphelinat à Vainc-Perchis",
+    questGiver: 'Brigand vétéran',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e3'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e5'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-04-22',
+    dateFin: '2022-04-27',
+    status: 'Unassigned',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('cabe64dcf0d4447fa60f5e13'),
+    name: 'A travers les bois	',
+    description:
+      "Séduire avec Primrose et la conduire à l'homme en vert dans la partie haute de Graben",
+    questGiver: 'Dame raffinée',
+    bounty: Math.floor(Math.random() * 1000),
+    requiredProfiles: [
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e3'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e2'),
+        experience: Math.floor(Math.random() * 10),
+      },
+      {
+        speciality: new mongoose.Types.ObjectId('5cabe64dcf0d4447fa60f5e5'),
+        experience: Math.floor(Math.random() * 10),
+      },
+    ],
+    awardedExperience: Math.floor(Math.random() * 10),
+    dateDebut: '2022-04-19',
+    dateFin: '2022-04-20',
+    status: 'Unassigned',
+  },
 ];
 
 export = requests.map((request) => {
@@ -141,10 +319,10 @@ export = requests.map((request) => {
       'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg',
     questGiver: request.questGiver,
     bounty: request.bounty,
-    dateFin: request.dateFin,
+    dateFin: new Date(request.dateFin),
     requiredProfiles: request.requiredProfiles,
     awardedExperience: request.awardedExperience,
-    dateDebut: request.dateDebut,
+    dateDebut: new Date(request.dateDebut),
     status: request.status,
   };
 });
